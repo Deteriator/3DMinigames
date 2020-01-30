e9408264a463d60be834245d0619bac923f890f8
Deteriator <markgriffith009@gmail.com>
2020-01-30 09:24:57 -0500

    Animations and turning in.

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,18 @@
-let scene,camera,renderer,mesh,light,grid,model,mixer,actions,clock;
+let scene,camera,renderer,mesh,light,grid,model,mixer,actions,clock,arcade;
+let walking = false;
 clock = new THREE.Clock()
+let keyboard = new THREEx.KeyboardState();
 
 let init = ()=>{
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0xe0e0e0 );
-	scene.fog = new THREE.Fog( 0xe0e0e0, 20, 100 );
+	scene.background = new THREE.Color( 0x1d0c29 );
+	scene.fog = new THREE.Fog( 0x3f0e40, 20, 100 );
 
 	//camera 
 
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 100 );
 	camera.position.set( - 5, 3, 10 );
 	camera.lookAt( new THREE.Vector3( 0, 2, 0 ) );
-
 
 	// lights
 	light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
@@ -22,9 +23,13 @@ let init = ()=>{
 	light.position.set( 0, 20, 10 );
 	scene.add( light );
 
+	light = new THREE.AmbientLight( 0xf27dd9 );
+	light.position.set( 0, 20, 10 );
+	scene.add( light );
+
 	// ground
 
-	mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x210378, depthWrite: false } ) );
+	mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x3c113d, depthWrite: false } ) );
 	mesh.rotation.x = - Math.PI / 2;
 	scene.add( mesh );
 
@@ -33,17 +38,12 @@ let init = ()=>{
 	grid.material.transparent = false;
 	scene.add( grid );
 
-
-
-
-
 	renderer = new THREE.WebGLRenderer({antialias: true} );
-	renderer.setClearColor( 0xffffff, 0 )
+	renderer.setClearColor( 0x3f0e40, 0.5 )
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 
-
-	camera.position.z = 7;
+	camera.position.z = 20;
 }
 
 init();
@@ -58,60 +58,89 @@ document.querySelector('button').addEventListener('click', function() {
 
 		// load a sound and set it as the Audio object's buffer
 		let audioLoader = new THREE.AudioLoader();
-		audioLoader.load( '../sounds/chill.ogg', function( buffer ) {
+		audioLoader.load( '../sounds/vapor.ogg', function( buffer ) {
 			sound.setBuffer( buffer );
 			sound.setLoop( true );
 			sound.setVolume( 1 );
 			sound.play();
 		});
 });
+let updateMovement = () =>{
+	let moveDistance = 50 * clock.getDelta();
 
-// instantiate the loader
+	if ( keyboard.pressed("W")) {
+		model.rotation.y = (Math.PI) 
+		model.translateZ( moveDistance );
+		mixer.clipAction(model.animations[10]).play()
+	}	
+	if ( keyboard.pressed("S")) {
+		model.rotation.y = Math.PI + Math.PI
+		model.translateZ( moveDistance );
+		mixer.clipAction(model.animations[10]).play()
+	}
+
+	if ( keyboard.pressed("A")) {
+		model.rotation.y = -Math.PI / 2;
+		model.translateZ( moveDistance );
+		mixer.clipAction(model.animations[10]).play()
+	}
+		
+	if ( keyboard.pressed("D")){
+		model.rotation.y = Math.PI / 2;
+		model.translateZ(  moveDistance );
+		mixer.clipAction(model.animations[10]).play()
+	}	
+}
+//  Roe Roe
+
+
+
+
+// Loaders for
 const objLoader = new THREE.OBJLoader();
 objLoader.setPath('../blender-files/')
 const mtlLoader = new THREE.MTLLoader();
 mtlLoader.setPath('../blender-files/')
 const gltfLoader = new THREE.GLTFLoader();
 gltfLoader.setPath('../blender-files/')
-//When materials are received 
 
+//Arcade Machine
+gltfLoader.load('arcade.glb', function (glb){
+arcade = glb.scene
+scene.add(arcade)
+arcade.scale.set(3.5,3.5,3.5)
+		arcade.position.z = -5
+		arcade.position.x = -1
+})
 
 //model added
+
 gltfLoader.load('RobotExpressive.glb', function (glb){
 	model = glb.scene
+	model.animations = glb.animations
 	scene.add(model)
 	mixer = new THREE.AnimationMixer( model );
-	console.log(glb.animations)
-	idle = mixer.clipAction(glb.animations[6])
+	idle = mixer.clipAction(glb.animations[2])
 	idle.play()
 })
-// movement - please calibrate these values
-let xSpeed = 0.01;
-let ySpeed = 0.01;
-
-document.addEventListener("keydown", onDocumentKeyDown, false);
-
-function onDocumentKeyDown(event) {
-    let keyCode = event.which;
-    if (keyCode == 87) {
-        model.position.y += ySpeed;
-    } else if (keyCode == 83) {
-        model.position.y -= ySpeed;
-    } else if (keyCode == 65) {
-        model.position.x -= xSpeed;
-    } else if (keyCode == 68) {
-        model.position.x += xSpeed;
-    } else if (keyCode == 32) {
-        model.position.set(0, 0, 0);
-    }
-    render();
-};
-
+function getDistance(mesh1, mesh2) { 
+  let dx = mesh1.position.x - mesh2.position.x; 
+  let dy = mesh1.position.y - mesh2.position.y; 
+  let dz = mesh1.position.z - mesh2.position.z; 
+  return Math.sqrt(dx*dx+dy*dy+dz*dz);
+}
+function interactable(mesh1,mesh2){
+   if( getDistance(mesh1,mesh2) <= 5){
+   	  console.log('can interact');
+   }
+}
 
 let render = () => {
 	let mixerUpdateDelta = clock.getDelta()
 	requestAnimationFrame( render );
 	mixer.update(mixerUpdateDelta)
 	renderer.render( scene, camera );
+	interactable(model,arcade);
+	updateMovement()
 }
 render();
